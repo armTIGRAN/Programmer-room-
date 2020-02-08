9a16140c49c95a79a2cc6d3c953543db65fcc511
armTIGRAN <tigran_badalyan_02@mail.ru>
2020-02-08 12:05:00 +0400

    heroku2

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-console.log("test Node");
+// const express=require('express')
+// const app= express()
 
-// const os = require('os');
-// console.log("Операционная система: ", os.platform());
-// console.log("архитектура процессор: ", os.arch());
-// console.log("инфа по процессорам: ", os.cpus());
-// console.log("свободная память: ", os.freemem());
+// const PORT = process.env.PORT || 80
 
-const lis = document.querySelectorAll("li");
-const a = document.querySelectorAll("li a");
+// app.get('/', (req, res) =>{
+//     res.end(`index.html`)
+// })
+
+// app.listen(PORT, () => {
+//     console.log('Server has been started');
+// })
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// Define the port to run on
+app.set('port', process.env.PORT || 80);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
