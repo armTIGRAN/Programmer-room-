9fa36e049b66742e3ae2d9f7ec5b35bce0697895
Tigran David <deulin717@gmail.com>
2020-02-24 19:40:27 +0400

    mode1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 
 // Define the port to run on
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8000);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,20 +16,23 @@ var server = app.listen(app.get('port'), function () {
 
 ///////////////////////////////////////////
 
-// var mysql = require('mysql');
+var mysql = require('mysql');
 
-// var connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'armTIGRAN',
-//     password: '5cf5c7ca60',
-//     database: 'programmerroom'
-// });
+var mysqlUser = 'G1xMeJNq45';
+var mysqlPassword = '7fFEISxz4p';
 
+var connection = mysql.createConnection({
+    host: 'remotemysql.com',
+    port: 3306,
+    user: mysqlUser,
+    password: mysqlPassword,
+    database: 'G1xMeJNq45'
+});
 
-// connection.connect((err) => {
-//     if (err) throw err;
-//     console.log("mysql connect");
-// });
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("mysql connect");
+});
 
 //create database
 // app.get('/createdb', (req, res) => {
@@ -42,39 +45,44 @@ var server = app.listen(app.get('port'), function () {
 // });
 
 // Create table
-// app.get('/createpoststable', (req, res) => {
-//     let sql = 'CREATE TABLE accounts(id int AUTO_INCREMENT, username VARCHAR(255), password VARCHAR(255), possition VARCHAR(255))';
-//     connection.query(sql, (err, result) => {
-//         if(err) throw err;
-//         console.log(result);
-//         res.send('Posts table created...');
-//     });
-// });
+app.get('/createpoststable', (req, res) => {
+    let sql = 'CREATE TABLE accounts(id int primary key AUTO_INCREMENT, username VARCHAR(255), password VARCHAR(255), position VARCHAR(255))';
+    connection.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Posts table created...');
+    });
+});
 
 // Insert post 1
-// app.get('/addpost1', (req, res) => {
-//     let post = {username:'b_tiko_a', password:'5cf5c7ca60p'};
-//     let sql = 'INSERT INTO accounts SET ?';
-//     let query = connection.query(sql, post, (err, result) => {
-//         if(err) throw err;
-//         console.log(result);
-//         res.send('Post 1 added...');
-//     });
-// });
+app.post('/addpost1', (req, res) => {
+    let post = {username:mysqlUser, password: mysqlPassword};
+    let sql = 'INSERT INTO accounts SET ?';
+    let query = connection.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Post 1 added...');
+    });
+});
+
+// Insert post 2
+app.post('/addpost2', (req, res) => {
+    let post = {username:mysqlUser, password: mysqlPassword};
+    let sql = 'INSERT INTO accounts SET ?';
+    let query = connection.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Post 2 added...');
+    });
+});
 
 // Select posts
-// app.get('/getposts', (req, res) => {
-//     let sql = 'SELECT * FROM accounts';
-//     let query = connection.query(sql, (err, results) => {
-//         if(err) throw err;
-//         console.log(results);
-//         res.send('Posts fetched...');
-//     });
-// });
 
-
-
-
-                                               
-
-
+app.get('/getposts', (req, res) => {
+    let sql = 'SELECT * FROM accounts';
+    let query = connection.query(sql, (err, results) => {
+        if(err) throw err;
+        // res.send('Posts fetched...');
+        res.send(results);
+    });
+});
